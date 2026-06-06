d04903777489c1cb0739b7f956f2c2be450ae690
Nx_erfan <erfansadeghi230@gmail.com>
2026-06-06 13:09:09 +0330

    kir

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-  output: 'export',
-  distDir: "dist",
-   trailingSlash: true,
-  allowedDevOrigins: ["192.168.1.101"],
-
-  experimental: {
-    turbopackFileSystemCacheForDev: false,
-  },
+  
 };
 
 export default nextConfig;
